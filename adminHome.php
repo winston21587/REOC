7425ded1cb95cf7b592b2a2c06dc7abfaded32ef
Winston <tabotabowinston@gmail.com>
2025-03-20 14:18:21 +0800

    a

--- adminHome.php
+++ adminHome.php
@@ -1,7 +1,8 @@
-<?php include 'Website Loading Screen/loader.php'; ?> <!-- call these for website loading animation -->
-<link rel="stylesheet" href="Website Loading Screen/loader.css"> <!-- call these for website loading animation -->
-<script src="Website Loading Screen/loader.js"></script> <!-- call these for website loading animation -->
-<?php
+<?php 
+// include 'Website Loading Screen/loader.php';  <!-- call these for website loading animation -->
+// <link rel="stylesheet" href="Website Loading Screen/loader.css"> <!-- call these for website loading animation -->
+// <script src="Website Loading Screen/loader.js"></script> <!-- call these for website loading animation -->
+
 
 session_start();
 include('updateAppointments.php');
